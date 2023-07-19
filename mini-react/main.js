029b86e6330a0f59874f1937821e91aefc74e0be
Matiyas-h <mattsis71@icloud.com>
2023-07-19 21:13:26 +0300

    implemented a simple render method that will display element on the browser from create element func

--- mini-react/main.js
+++ mini-react/main.js
@@ -1,19 +1,9 @@
-import { MiniReact } from './miniReact';
+import { MiniReact } from './miniReact.js';
 
-// the process of creating element representation.
 const element = MiniReact.createElement(
-  'div', // types or node value
-  { id: 'foo' }, // props
-  'Hello', // childeren
-  MiniReact.createElement('span', 'null', 'world'), // this is nested element in the above elemenet 
-)
-
-
-// const element_two = MiniReact.createElement(
-//   "div",
-//   { className: "greeting" },
-//   "h1",
-//   null,
-//   "Hello world!"
-// )
-console.log(element);
+  "div",
+  { id: "foo" },
+  "Hellow there"
+);
+const container = document.getElementById("root");
+MiniReact.render(element, container);
